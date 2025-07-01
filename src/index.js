ab03a700cccbd75cf0f4f35b7d88017c3f793342
Sufyan-Pawaskar <125754147+Sufyan-Pawaskar@users.noreply.github.com>
2025-07-01 13:45:55 +0000

    updated the basic structure for the intial first load without content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css"
+import logo from "./assets/logo.png"
+
+const navLogo = document.getElementById("logoEle");
+console.log(navLogo)
+
+const logoImg = document.createElement("img");
+logoImg.src = logo;
+logoImg.classList.add("logoImg");
+
+
+
+navLogo.appendChild(logoImg)
