ebce2b96777f6ac2e7c9ca555a86331c4ada60b7
Sufyan-Pawaskar <125754147+Sufyan-Pawaskar@users.noreply.github.com>
2025-07-01 18:45:17 +0000

    update home.js

--- src/home.js
+++ src/home.js
@@ -1,1 +1,8 @@
-export default loadHomepage = 
+export default loadHomepage = function(){
+    const contentDiv = document.getElementById("content");
+    const children = contentDiv.childNodes;
+    for(let i = children.length; i > -1; i--){
+        contentDiv.removeChild(children[i])
+    }
+    
+}
